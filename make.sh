1b4962b90df8871642ea2508ae2b1ac00d4e50f4
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-09-07 11:44:57 -0600

    update scripts

--- make.sh
+++ make.sh
@@ -8,12 +8,12 @@
 # Calling functions by name on the command line shouldn't be affected by these.
 
 
-# BuildAllBinariesRunAllTests=1
+BuildAllBinariesRunAllTests=1
 
-RunPreemptivePoof=1
+# RunPreemptivePoof=1
 
 # RunPoof=1
-BuildPoof=1
+# BuildPoof=1
 # POOF_DEBUGGER="gdb --args"
 # POOF_LOG_LEVEL="--log-level LogLevel_Debug"
 
