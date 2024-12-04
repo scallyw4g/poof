32ecfd4372620e6cb2a277ace1ea2f54db011c92
a pirate <jesse.hughes.it@gmail.com>
2024-12-04 10:35:39 -0700

    Rebuild

--- docs/poof_runtime.js
+++ docs/poof_runtime.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {}
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\jesse\AppData\Local\Temp\tmpxnevm17p.js
+// include: C:\Users\jesse\AppData\Local\Temp\tmp7hhib40z.js
 Module["expectedDataFileDownloads"] ??= 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -349,23 +349,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: C:\Users\jesse\AppData\Local\Temp\tmpxnevm17p.js
-// include: C:\Users\jesse\AppData\Local\Temp\tmpcf00sjt4.js
+// end include: C:\Users\jesse\AppData\Local\Temp\tmp7hhib40z.js
+// include: C:\Users\jesse\AppData\Local\Temp\tmpospdbzuj.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if (Module["$ww"] || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\jesse\AppData\Local\Temp\tmpcf00sjt4.js
-// include: C:\Users\jesse\AppData\Local\Temp\tmp2w5n2_xx.js
+// end include: C:\Users\jesse\AppData\Local\Temp\tmpospdbzuj.js
+// include: C:\Users\jesse\AppData\Local\Temp\tmp3uw11z31.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\jesse\AppData\Local\Temp\tmp2w5n2_xx.js
+// end include: C:\Users\jesse\AppData\Local\Temp\tmp3uw11z31.js
 // Sometimes an existing Module object exists with properties
 // meant to overwrite the default module functionality. Here
 // we collect those properties and reapply _after_ we configure
